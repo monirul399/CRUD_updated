41c76660fe2f77f17a80994168d1d720fa1c7329
monirul <monirul@mislbd.com>
2021-06-28 15:56:51 +0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { StudentService } from './student.service';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule, FormsModule
+  ],
+  providers: [StudentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
